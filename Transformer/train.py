a40fa5ab178621fc508da133a5939121e674fd12
Teabue <danina100000@gmail.com>
2024-03-20 23:26:17 +0100

    added configs for training

--- Transformer/train.py
+++ Transformer/train.py
@@ -5,22 +5,46 @@ import yaml
 import numpy as np
 import torch.nn as nn
 from utils.model import Transformer
-from utils.data_loader import TransformerDataset  
+from utils.data_loader_2 import TransformerDataset  
 from utils.vocabularies import Vocabulary
 
 
 
-def train_model(model, criterion, optimizer, dataset: TransformerDataset, num_epochs=100, device='cuda', batch_size=20, nr_pitches=128, tgt_vocab_size=None):
+def train_model(model, 
+                criterion, 
+                optimizer, 
+                dataset: TransformerDataset, 
+                num_epochs=100, 
+                device='cuda', 
+                batch_size=20, 
+                tgt_vocab_size=None, 
+                run_save_path='', 
+                save_model_every_num_epoch=1):
     
-
+    
+    MODEL_SAVE_DIR = os.path.join(run_save_path, "models")
+    os.makedirs(MODEL_SAVE_DIR, exist_ok=True)
+    
+    LOSS_SAVE_DIR = os.path.join(run_save_path, "losses")
+    os.makedirs(LOSS_SAVE_DIR, exist_ok=True)
+    
+    TRAIN_LOSS_PATH = os.path.join(LOSS_SAVE_DIR, 'train_losses.txt')
+    VAL_LOSS_PATH = os.path.join(LOSS_SAVE_DIR, 'val_losses.txt')
+    
+    with open(TRAIN_LOSS_PATH, 'w') as f:
+        f.write('Epoch, Loss\n')
+    with open(VAL_LOSS_PATH, 'w') as f:
+        f.write('Epoch, Loss\n')
+    
+    best_loss = float('inf')
     for epoch in range(num_epochs):
-        
+        # -------------------------------- Train model ------------------------------- #
         train_loader = dataset.get_split('train', batch_size=batch_size, shuffle=True)
         pbar = tqdm.tqdm(train_loader, total = len(train_loader), \
-                         desc=f'Train: Loss: [{1}], Epochs: {epoch}/{num_epochs}', leave = False)    
-
-        losses = []
-        for spectrograms, tokens in pbar:#iter(train_loader):
+                         desc=f'Train: Loss: [{1}], Epochs: {epoch}/{num_epochs}, GPU Memory: {0}GB', leave = False)    
+        model.train()
+        train_losses = []
+        for spectrograms, tokens in pbar:
             spectrograms = spectrograms.to(device)
             tokens = tokens.to(device)
             
@@ -32,19 +56,35 @@ def train_model(model, criterion, optimizer, dataset: TransformerDataset, num_ep
             loss.backward()
             
             optimizer.step()
+                        
+            if device.type == 'cuda':
+                pbar.set_description(f'Train Loss: [{loss.item():.4f}], Epochs: {epoch}/{num_epochs}, GPU Memory: {torch.cuda.memory_allocated()/1e9:.2f}GB')
+            else:
+                pbar.set_description(f'Train Loss: [{loss.item():.4f}], Epochs: {epoch}/{num_epochs}')
+                
+            train_losses.append(loss.item())
             
-            pbar.set_description(f'Loss: [{loss.item():.4f}], Epochs: {epoch}/{num_epochs}')
-            losses.append(loss.item())
+            # Me trying to free memory because for some reason, the memory usage keeps increasing
+            del output, loss, spectrograms, tokens
         
-        print(f'Train res; Epoch: {epoch}, Loss: {sum(losses)/len(losses)}')
-
-        test_loader = dataset.get_split('val', batch_size=batch_size, shuffle=True)
-        pbar = tqdm.tqdm(iter(test_loader), total = len(test_loader), \
+        mean_train_loss = np.mean(train_losses)
+        print(f'Train res; Epoch: {epoch}, Loss: {mean_train_loss}')
+        
+        # Save training loss
+        with open(TRAIN_LOSS_PATH, 'a') as f:
+            f.write(f'{epoch}, {mean_train_loss}\n')
+            
+        if epoch % save_model_every_num_epoch == 0:
+            torch.save(model.state_dict(), os.path.join(MODEL_SAVE_DIR, f'checkpoint_model.pth'))
+            
+        # ------------------------------ Validate model ------------------------------ #
+        val_loader = dataset.get_split('val', batch_size=batch_size, shuffle=True)
+        pbar = tqdm.tqdm(iter(val_loader), total = len(val_loader), \
                         desc=f'Loss: [{1}], Epochs: {epoch}/{num_epochs}')
         
-        best_loss = float('inf')
-        losses = []
-        correct = 0
+        model.eval()
+        
+        val_losses = []
         for i, (spectrograms, tokens) in enumerate(pbar):
             spectrograms = spectrograms.to(device)
             tokens = tokens.to(device)
@@ -54,28 +94,40 @@ def train_model(model, criterion, optimizer, dataset: TransformerDataset, num_ep
             outputs = model(src=spectrograms, tgt=tokens)
             
             loss = criterion(outputs.contiguous().view(-1, tgt_vocab_size), tokens.contiguous().view(-1))
+            if device.type == 'cuda':
+                pbar.set_description(f'Validation Loss: [{loss.item():.4f}], Epochs: {epoch}/{num_epochs}, GPU Memory: {torch.cuda.memory_allocated()/1e9:.2f}GB')
+            else:
+                pbar.set_description(f'Validation Loss: [{loss.item():.4f}], Epochs: {epoch}/{num_epochs}')
+            val_losses.append(loss.item())
             
-            pbar.set_description(f'Validation: Loss: [{loss.item():.4f}], Epochs: {epoch}/{num_epochs}')
-            losses.append(loss.item())
-            
+            # Don't mind me trying to free memory
+            del outputs, loss, spectrograms, tokens
+        
         # Save model if loss is better
-        mean_loss = np.mean(losses)
-        if mean_loss < best_loss:
-            best_loss = mean_loss
-            MODEL_SAVE_DIR = os.path.join('/work3/s214629', "models")
-            os.makedirs(MODEL_SAVE_DIR, exist_ok=True)
-            torch.save(model.state_dict(), os.path.join(MODEL_SAVE_DIR, f'model_best.pth'))  
+        mean_val_loss = np.mean(val_losses)
+        
+        # Save validation loss
+        with open(VAL_LOSS_PATH, 'a') as f:
+            f.write(f'{epoch}, mean_val_loss\n')
+        
+
+        if mean_val_loss < best_loss:
+            best_loss = mean_val_loss
+            torch.save(model.state_dict(), os.path.join(MODEL_SAVE_DIR, f'model_best_epoch-{epoch}.pth'))  
 
 
-def simple_setup(lr = 0.01, device = 'cuda', nr_pitches=128, datadir = 'data_process/output/segments'):
-    #TODO: Make this into configs
-    n_mel_bins = 128
-    d_model = 512
-    num_heads = 8
-    num_layers = 6
-    d_ff = 2048
-    max_seq_length = 1024 # doesn't really matter, can be set to whatever as long as its larger than the longest sequence - it's a pteprocess step for PE
-    dropout = 0.1
+def simple_setup(device = 'cuda', 
+                datadir = 'data_process/output/segments',
+                lr = 0.0001,  
+                n_mel_bins = 128, 
+                d_model = 512,
+                num_heads = 8,
+                num_layers = 6,
+                d_ff = 2048,
+                max_seq_length = 1024, # doesn't really matter, can be set to whatever as long as its larger than the longest sequence - it's a pteprocess step for PE
+                dropout = 0.1):
+
+    # TODO: Temporary solution, way into the future when I actually feel like refactoring, I'll save config run files
     with open('Transformer/configs/vocab_config.yaml', 'r') as file:
         config = yaml.safe_load(file)
     vocab = Vocabulary(config)
@@ -84,20 +136,49 @@ def simple_setup(lr = 0.01, device = 'cuda', nr_pitches=128, datadir = 'data_pro
     tgt_vocab_size = vocab.vocab_size
     
     model = Transformer(n_mel_bins, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout, device)
-    model.train()
     model.to(device)
     
     criterion = nn.CrossEntropyLoss(ignore_index=0) # we ignore the padding token
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.98), eps=1e-9)
     dataset = TransformerDataset(datadir)
     
     return model, criterion, optimizer, dataset, tgt_vocab_size
 
 
 if __name__ == '__main__':
+    """ Run the file from the repo root folder"""
+    import yaml
+    import shutil
+    
+    # Load the YAML file
+    with open("Transformer/configs/train_config.yaml", 'r') as f:
+        config = yaml.safe_load(f)
+    
+    # Copy the config file to the run_save_path
+    os.makedirs(config['run_save_path'], exist_ok=True)
+    shutil.copy("Transformer/configs/train_config.yaml", os.path.join(config['run_save_path'], "train_config.yaml"))
+    
+    
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(">>>>> Using device: ", DEVICE)
-    model, criterion, optimizer, dataset, tgt_vocab_size = simple_setup(device = DEVICE, datadir = '/work3/s214629/preprocessed_data')
+    model, criterion, optimizer, dataset, tgt_vocab_size = simple_setup(device = DEVICE, 
+                                                                        datadir = config['data_dir'], 
+                                                                        lr = config['lr'], 
+                                                                        n_mel_bins = config['n_mel_bins'], 
+                                                                        d_model = config['d_model'], 
+                                                                        num_heads = config['num_heads'], 
+                                                                        num_layers = config['num_layers'], 
+                                                                        d_ff = config['d_ff'], 
+                                                                        max_seq_length = config['max_seq_length'], 
+                                                                        dropout = config['dropout'])
     
-    train_model(model, criterion, optimizer, dataset, num_epochs=20, device=DEVICE, batch_size=150, tgt_vocab_size=tgt_vocab_size)
+    train_model(model, 
+                criterion, 
+                optimizer, 
+                dataset, 
+                num_epochs=config['num_epochs'], 
+                device=DEVICE, 
+                batch_size=config['batch_size'], 
+                tgt_vocab_size=tgt_vocab_size,
+                run_save_path=config['run_save_path'])
